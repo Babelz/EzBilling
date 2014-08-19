cfaa8179108811d156a6a1e47c09da54f6593d66
siquel <squual@kahvipaussi.net>
2014-08-19 18:15:01 +0300

    Added new database.

---
+++ EzBilling/DatabaseObjects/EzBillingDatabase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace EzBilling.DatabaseObjects
+{
+    public sealed class EzBillingDatabase
+    {
+        #region Vars
+        private readonly DatabaseHelper database;
+        #endregion
+
+        #region Ctor
+        public EzBillingDatabase()
+        {
+            database = new DatabaseHelper("db");
+        }
+        #endregion
+
+
+        #region Methods
+
+        public List<ClientInformation> GetClientInformations()
+        {
+            database.Open();
+            List<ClientInformation> r = new List<ClientInformation>();
+            DataTable table = database.Select("select * from client");
+            foreach (DataRow row in table.Rows)
+            {
+                ClientInformation c = new ClientInformation();
+                c.ID = row["client_id"].ToString();
+                c.Name = row["name"].ToString();
+                c.PostalCode = row["postal_code"].ToString();
+                c.Street = row["address"].ToString();
+                c.City = row["city"].ToString();
+                r.Add(c);
+            }
+
+            database.Close();
+            return r;
+        }
+
+
+        #endregion
+    }
+}
